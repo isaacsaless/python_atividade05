3977c35e2e130fd84b7c0d95845376daa83c129a
isaacsaless <isaacnascimentosales@gmail.com>
2023-10-11 11:11:58 -0300

    Primeira versão do código de conversão de metragem

---
+++ metragem.py
@@ -0,0 +1,7 @@
+# Digite uma metragem e veja isso em cm e mm:
+
+print('Descubra o valor de uma metragem em tanto em centímetros quanto em milímetros')
+metragem = float(input('Digite uma metragem em metros(m): '))
+cm = metragem*100
+mm = metragem*1000
+print(f'O valor que foi fornecido, quando convertido para centimetros é igual a: {cm}.\nO valor que foi fornecido, quando convertido para milimetros é igual a: {mm}.')
